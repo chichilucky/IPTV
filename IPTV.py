3fc41028f3ba24fedc15df7fc2ce959108ad62ec
chichilucky <82249389+chichilucky@users.noreply.github.com>
2024-01-16 14:59:07 +0800

    Add files via upload

--- IPTV.py
+++ IPTV.py
@@ -1,16 +1,16 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.chrome.options import 选项
+from selenium.webdriver.chrome.options import Options
 import requests
 import json
 import re
 
-liaoning = "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i6L695a6BIg%3D%3D"    #辽宁
-zhejiang = "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rWZ5rGfIg%3D%3D"    #浙江
+shaanxi = "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i6L695a6BIg%3D%3D"    #陕西
+hunan = "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rWZ5rGfIg%3D%3D"    #湖南
 
 def process_url(url):
     # 创建一个Chrome WebDriver实例
-    chrome_options = 选项()
+    chrome_options = Options()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
@@ -29,11 +29,11 @@ def process_url(url):
     pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
     urls_all = re.findall(pattern, page_content)
     urls = list(set(urls_all))  # 去重得到唯一的URL列表
-    for url 在 urls:
+    for url in urls:
         print(url)
     # 遍历网址列表，获取JSON文件并解析
     results = []
-    for url 在 urls:
+    for url in urls:
         try:
             # 发送GET请求获取JSON文件，设置超时时间为5秒
             json_url = f"{url}/iptv/live/1000.json?key=txiptv"
@@ -41,48 +41,48 @@ def process_url(url):
             json_data = response.json()
 
             # 解析JSON文件，获取name和url字段
-            for item 在 json_data['data']:
+            for item in json_data['data']:
                 if isinstance(item, dict):
                     name = item.get('name')
                     urlx = item.get('url')
                     urld = f"{url}{urlx}"
 
-                    if name 和 urlx:
+                    if name and urlx:
                         # 删除特定文字
-                        name = name.replace("中央"， "CCTV")
-                        name = name.replace("高清"， "")
-                        name = name.replace("标清"， "")
-                        name = name.replace("频道"， "")
-                        name = name.replace("-"， "")
-                        name = name.replace(" "， "")
-                        name = name.replace("PLUS"， "+")
-                        name = name.replace("("， "")
-                        name = name.replace(")"， "")
+                        name = name.replace("中央", "CCTV")
+                        name = name.replace("高清", "")
+                        name = name.replace("标清", "")
+                        name = name.replace("频道", "")
+                        name = name.replace("-", "")
+                        name = name.replace(" ", "")
+                        name = name.replace("PLUS", "+")
+                        name = name.replace("(", "")
+                        name = name.replace(")", "")
 
-                        name = name.replace("CCTV1综合"， "CCTV1")
-                        name = name.replace("CCTV2财经"， "CCTV2")
-                        name = name.replace("CCTV3综艺"， "CCTV3")
-                        name = name.replace("CCTV4国际"， "CCTV4")
-                        name = name.replace("CCTV5体育"， "CCTV5")
-                        name = name.replace("CCTV6电影"， "CCTV6")
-                        name = name.replace("CCTV7军事"， "CCTV7")
-                        name = name.replace("CCTV7军农"， "CCTV7")
-                        name = name.replace("CCTV8电视剧"， "CCTV8")
-                        name = name.replace("CCTV9记录"， "CCTV9")
-                        name = name.replace("CCTV9纪录"， "CCTV9")
-                        name = name.replace("CCTV10科教"， "CCTV10")
-                        name = name.replace("CCTV11戏曲"， "CCTV11")
-                        name = name.replace("CCTV12社会与法"， "CCTV12")
-                        name = name.replace("CCTV13新闻"， "CCTV13")
-                        name = name.replace("CCTV14少儿"， "CCTV14")
-                        name = name.replace("CCTV15音乐"， "CCTV15")
-                        name = name.replace("CCTV16奥林匹克"， "CCTV16")
+                        name = name.replace("CCTV1综合", "CCTV1")
+                        name = name.replace("CCTV2财经", "CCTV2")
+                        name = name.replace("CCTV3综艺", "CCTV3")
+                        name = name.replace("CCTV4国际", "CCTV4")
+                        name = name.replace("CCTV5体育", "CCTV5")
+                        name = name.replace("CCTV6电影", "CCTV6")
+                        name = name.replace("CCTV7军事", "CCTV7")
+                        name = name.replace("CCTV7军农", "CCTV7")
+                        name = name.replace("CCTV8电视剧", "CCTV8")
+                        name = name.replace("CCTV9记录", "CCTV9")
+                        name = name.replace("CCTV9纪录", "CCTV9")
+                        name = name.replace("CCTV10科教", "CCTV10")
+                        name = name.replace("CCTV11戏曲", "CCTV11")
+                        name = name.replace("CCTV12社会与法", "CCTV12")
+                        name = name.replace("CCTV13新闻", "CCTV13")
+                        name = name.replace("CCTV14少儿", "CCTV14")
+                        name = name.replace("CCTV15音乐", "CCTV15")
+                        name = name.replace("CCTV16奥林匹克", "CCTV16")
 
-                        name = name.replace("CCTV17农业农村"， "CCTV17")
-                        name = name.replace("CCTV5+体育赛视"， "CCTV5+")
-                        name = name.replace("CCTV5+体育赛事"， "CCTV5+")
-                        results.append(f"{name}，{urld}")
-        except requests.exceptions。RequestException as e:
+                        name = name.replace("CCTV17农业农村", "CCTV17")
+                        name = name.replace("CCTV5+体育赛视", "CCTV5+")
+                        name = name.replace("CCTV5+体育赛事", "CCTV5+")
+                        results.append(f"{name},{urld}")
+        except requests.exceptions.RequestException as e:
             print(f"Failed to process JSON for URL {json_url}. Error: {str(e)}")
             continue
         except json.JSONDecodeError as e:
@@ -93,13 +93,13 @@ def process_url(url):
 def save_results(results, filename):
     # 将结果保存到文本文件
     with open(filename, "w", encoding="utf-8") as file:
-        for result 在 results:
+        for result in results:
             file.write(result + "\n")
             print(result)
 
-results_liaoning = process_url(liaoning)
-save_results(results_liaoning, "liaoning.txt")
+results_shaanxi = process_url(shaanxi)
+save_results(results_shaanxi, "shaanxi.txt")
 
-results_zhejiang = process_url(zhejiang)
-save_results(results_zhejiang, "zhejiang.txt")
+results_hunan = process_url(hunan)
+save_results(results_hunan, "hunan.txt")
 
